7394f79c105c4982cdc6c11ca4f0760473ea5f57
rhhill1 <146479249+rhhill1@users.noreply.github.com>
2024-05-13 14:17:37 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-
-player.say(":)")
+def on_on_chat():
+    # % highlight
+    for i in range(100):
+        mobs.spawn(CHICKEN, pos(0, 10, 0))
+player.on_chat("chicken", on_on_chat)
--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-player.say(":)")
+player.onChat("chicken", function on_on_chat() {
+    //  % highlight
+    for (let i = 0; i < 100; i++) {
+        mobs.spawn(CHICKEN, pos(0, 10, 0))
+    }
+})
